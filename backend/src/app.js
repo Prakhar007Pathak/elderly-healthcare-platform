3bb63fc54edc4ca548b5317408772df42893fa97
Prakhar Pathak <prakharpathak200@gmail.com>
2026-03-03 18:40:50 +0530

    setting up cors()

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,13 @@ const errorHandler = require('./middlewares/error.middleware');
 const adminRoutes = require('./routes/admin.routes');
 const ratingRoutes = require("./routes/rating.routes");
 
-const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://eldercare-delta.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/patients', patientRoutes);
